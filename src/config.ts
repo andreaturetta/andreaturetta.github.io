c32f05059fa856710ce4d75f5dea1c4d844a8652
andreaturetta <turettaandrea201@gmail.com>
2026-01-13 01:14:33 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -11,7 +11,21 @@ export const siteConfig = {
   },
   aboutMe:
     "Sono uno studente di Cybersecurity con una forte passione per l’informatica e le tecnologie digitali, nata fin da piccolo con i videogame e l’abitudine a smontare e riparare computer e telefoni. Questa curiosità si è trasformata in un approccio pratico orientato a capire a fondo il funzionamento dei sistemi. Sono motivato a mettere in pratica in ambito professionale le competenze che sto acquisendo nel networking e nei sistemi di rete, contribuendo ai progetti e crescendo attraverso l’esperienza sul campo.",
-  skills: ["Java", "AI", "Office", "Virtualizzazione", "GIT", "Docker", "PhpMyAdmin", "NAS", "HTML5", "CSS", "Sistemi operativi", "Networking", "Assemblaggio PC"],
+  skills: [
+    "Java",
+    "AI",
+    "Office",
+    "Virtualizzazione",
+    "GIT",
+    "Docker",
+    "PhpMyAdmin",
+    "NAS",
+    "HTML5",
+    "CSS",
+    "Sistemi operativi",
+    "Networking",
+    "Assemblaggio PC",
+  ],
   projects: [
     {
       name: "BuildYourOwn",
@@ -40,25 +54,21 @@ export const siteConfig = {
       company: "Work in progress...",
       title: "Lavorando duramente per riempire questa pagina....",
       dateRange: "Present - ....Future",
-      bullets: [ 
-        "",
-    ],
-  },
-  {
-      company: "...",
-      title: "...",
-      dateRange: "...",
       bullets: [
-        "...",
+        "In cerca di opportunità per fare esperienza pratica in ambito Cybersecurity e networking.",
       ],
     },
     {
       company: "...",
       title: "...",
+      dateRange: "...",
+      bullets: ["..."],
+    },
+    {
+      company: "...",
+      title: "...",
       dateRange: "....",
-      bullets: [
-        "...",
-      ],
+      bullets: ["..."],
     },
   ],
   education: [
@@ -66,15 +76,15 @@ export const siteConfig = {
       school: "ITS Digital Academy Mario Volpato",
       degree: "Diploma Tecnico Superiore sulla Cyber Sicurezza",
       dateRange: "2025 - 2028",
-      achievements: ["...",
-    ],
+      achievements: ["..."],
     },
     {
       school: "I.I.S. Leonardo Da Vinci",
-      degree: "Diploma di Maturità in Servizi Commerciali Web & per la community",
+      degree:
+        "Diploma di Maturità in Servizi Commerciali Web & per la community",
       dateRange: "2019 - 2025",
-      achievements: ["...",
-    ],
+      achievements: ["..."],
     },
   ],
 };
+
